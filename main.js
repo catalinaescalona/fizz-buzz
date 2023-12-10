3d95235a093aba2965b41e055da0db121b2ff6c4
Catalina Escalona <catalinaescalona@gmail.com>
2023-12-10 11:40:10 +0100

    header comments updated

---
+++ main.js
@@ -0,0 +1,9 @@
+// FizzBuzz Project
+// Created by Catalina Escalona, Winter Semester 2023-24
+// Course: "Web Coding: Intro to Web Development and Game Prototyping"
+// University of Applied Arts Vienna 
+
+// a function to determine if a number is divisible by 3, 5, or 3 and 5
+function fizzbuzz (num) {
+
+}
