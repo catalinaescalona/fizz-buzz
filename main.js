16e67dab6d97e5964e5d7b1fb0f8905880b76c29
Catalina Escalona <catalinaescalona@gmail.com>
2023-12-11 15:01:05 +0100

    created start and stop buttons with images

--- main.js
+++ main.js
@@ -4,8 +4,9 @@
 // University of Applied Arts Vienna 
 
 $(document).ready(function() {
-    // initialize counter variable
+    // initialize variables
     let counter = 0;
+    let counterInterval;
 
     // a function to determine if a number is divisible by 3, 5, or 3 and 5
     function fizzbuzz(num) {
@@ -39,15 +40,15 @@ $(document).ready(function() {
     $("#count").on("click", function() {
         counter += 1;
         $("#number").text((counter).toString());
-
         let fizzbuzz_result = fizzbuzz(counter);
-        console.log(fizzbuzz_result);
+        // console.log(fizzbuzz_result);
         fizzbuzz_img(fizzbuzz_result);
     });
 
     // a function to display corrresponding fizzbuzz image
     function fizzbuzz_img(result) {
         let image_id;
+
         if (result === "fizz") {
             image_id = result + "-img";
         } else if (result === "buzz") {
@@ -55,14 +56,64 @@ $(document).ready(function() {
         } else if (result === "fizzbuzz") {
             image_id = result + '-img';
         }
+
         let image = $("#" + image_id);
+        let number = $("#number");
+
+        // calculate center position for image
+        let centerX = number.offset().left + number.width() / 2 - image.width() / 2;
+        let centerY = number.offset().top + number.height() / 2 - image.height() / 2;
+
+        // set image position at center of number
+        image.css({
+            position: "absolute",
+            top: centerY,
+            left: centerX,
+            display: "none"
+        });
+
+        // show image with fade-in and fade-out effect
         image.fadeIn(1000, function() {
-            $(this).fadeOut(1000);
+            $(this).fadeOut(1000, function() {
+                $(this).css("display", "none");
+            });
         });
     }
 
     // clicking reset button resets display number to 0
     $("#reset").on("click", function() {
         $("#number").text((counter=0).toString());
+    });
+
+    // clicking start button automatically increases display number by 1 every 1 second
+    $("#start").on("click", function() {
+        counterInterval = setInterval(function() {
+            counter += 1;
+            $("#number").text(counter.toString());
+            let fizzbuzz_result = fizzbuzz(counter);
+            // console.log(fizzbuzz_result);
+            fizzbuzz_img(fizzbuzz_result);
+        }, 1000);
+    });
+
+    // clicking stop button stops the counter that was initiated by start button
+    $("#stop").on("click", function() {
+        clearInterval(counterInterval);
+    });
+
+    // added click effect so user gets visual feedback when they click button
+    $("button").on({
+        mousedown: function() {
+            $(this).css({
+                color: "white",
+                backgroundColor: "plum"
+            });
+        },
+        mouseup: function() {
+            $(this).css({
+                color: "",
+                backgroundColor: ""
+            });
+        }
     });
 });
